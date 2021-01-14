f781fc7c3b4bc3276da986ed6cab8d3fc5bd9b7f
Musabikov <58425663+Musabikov@users.noreply.github.com>
2021-01-14 11:34:55 +0500

    hi

---
+++ Variant5/Variant5/Variant5/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Variant5
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var text = new[]
+            {
+                "Текст. представлен. в виде массива строк, слова в которых; разделены: пробелами-и знаками",
+                "Новый, массив; должен содержать не: более- n таких слов. Все слова должны быть в нижнем регистре."
+            };
+
+            var words = GetLongestWords(text, 5);
+            foreach (var word in words)
+                Console.WriteLine(word);
+
+            var strs = File.ReadAllLines("db.txt");
+            var dataBase = new List<Record>();
+
+            foreach (var str in strs)
+            {
+                var data = str.Split();
+
+                var record = new Record()
+                {
+                    ClientID = int.Parse(data[0]),
+                    Year = int.Parse(data[1]),
+                    Month = int.Parse(data[2]),
+                    Duration = int.Parse(data[3])
+                };
+
+                dataBase.Add(record);
+            }
+
+            PrintYearsLongestDurationOfMonth(dataBase, 2);
+            Console.ReadKey();
+        }
+
+        public static string[] GetLongestWords(string[] lines, int n)
+        {
+            return lines.SelectMany(x => x.Split(new[] {'-', ':', ',', '.', ' ', ';'}))
+                .OrderBy(x => x)
+                .ThenByDescending(x => x.Length)
+                .Take(n)
+                .ToArray();
+        }
+
+        public static void PrintYearsLongestDurationOfMonth(List<Record> data, int id)
+        {
+            var years = data.Where(x => x.ClientID == id)
+                .GroupBy(x => x.Year)
+                .Select(x => (x.Max(y => (y.Duration, y.Month)), x.Key))
+                .OrderByDescending(x => x.Key)
+                .ToArray();
+            if (years.Length > 0)
+            {
+                foreach (var year in years)
+                {
+                    Console.WriteLine(
+                        $"Год: {year.Key}, месяц: {year.Item1.Month}, продолжительность: {year.Item1.Duration}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Нет данных о клиенте");
+            }
+        }
+    }
+}
---
+++ Variant5/Variant5/Variant5/Record.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variant5
+{
+    public struct Record
+    {
+        public int ClientID; // идентификационный номер клиента 
+        public int Year;     // год 
+        public int Month;    // номер месяца 
+        public int Duration; // продолжительность занятий в данном месяце 
+        // данного года (в часах) }
+    }
+}
